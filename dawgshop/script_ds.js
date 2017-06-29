692cf2e0f72780ec42305ed347f85d86574e3eb0
EricaYuHackCamp <kumakamisama001@gmail.com>
2017-06-29 13:07:15 +0000

    Dawg It Up

--- dawgshop/script_ds.js
+++ dawgshop/script_ds.js
@@ -1,1 +1,5 @@
-document.getElementById('go').onclick = function() {}
+document.getElementById('go').onclick = function() {
+    var inputText = $('#text-input').val();
+    var dawgifiedText = inputText + ", dawg";
+    document.getElementById('input').innerHTML = dawgifiedText;
+};
